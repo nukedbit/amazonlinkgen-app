9e3ec594f6eeed51448050bd5e3931eca0a250c9
nukedbit <1690316+nukedbit@users.noreply.github.com>
2019-07-24 20:37:38 +0200

    Updates

---
+++ precache-manifest.140eea580f73f10a6825aa519f5a430c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a5eb4a474f25413b4a1d8866663450",
+    "url": "/amazonlinkgen-app/index.html"
+  },
+  {
+    "revision": "fdfba7387d79883b8540",
+    "url": "/amazonlinkgen-app/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "dc14b4bee194c7579611",
+    "url": "/amazonlinkgen-app/static/js/2.e0ce43a0.chunk.js"
+  },
+  {
+    "revision": "fdfba7387d79883b8540",
+    "url": "/amazonlinkgen-app/static/js/main.84884334.chunk.js"
+  },
+  {
+    "revision": "884dfa4fe6753d1a71ec",
+    "url": "/amazonlinkgen-app/static/js/runtime~main.afafadf7.js"
+  },
+  {
+    "revision": "2a3800a7327c9cb740fd5563b758389a",
+    "url": "/amazonlinkgen-app/static/media/amazon-link-builder.2a3800a7.png"
+  }
+]);
